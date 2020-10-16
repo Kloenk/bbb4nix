8be551cb0e79ed7f1affb73df76802f6d6e14f58
Finn Behrens <me@kloenk.de>
2020-10-16 14:08:54 +0200

    change packages to an overlay

---
+++ packages/overlay.nix
@@ -0,0 +1,40 @@
+final: prev:
+let inherit (final) callPackage libopusenc;
+in {
+  bbbPackages = {
+    akkaApps = callPackage ./bbb-akka-apps { };
+    akkaFsesl = callPackage ./bbb-akka-fsesl { };
+    web = callPackage ./bbb-web { };
+    blankSlides = callPackage ./bbb-blank-slides { };
+    freeswitchConfig = callPackage ./bbb-freeswitch-config { };
+    webrtcSfu = callPackage ./bbb-webrtc-sfu { };
+    greenlight = callPackage ./bbb-greenlight { };
+    etherpad-lite = callPackage ./bbb-etherpad-lite { };
+    html5 = callPackage ./bbb-html5/wrapper.nix { };
+    html5-unwrapped = callPackage ./bbb-html5 { };
+    recordAndPlaybackPresentation =
+      callPackage ./bbb-record-and-playback-presentation { };
+    generateSecrets = callPackage ./bbb-generate-secrets { };
+  };
+
+  kurentoPackages = {
+    kurento-media-server = callPackage ./kurento-media-server { };
+    kms-core = callPackage ./kms-core { };
+    kms-elements = callPackage ./kms-elements { };
+    kms-filters = callPackage ./kms-filters { };
+    gst_all_1 = callPackage ./kms-gst { };
+  };
+
+  freeswitchPackages = rec {
+    sofia_sip = callPackage ./sofia-sip { };
+    spandsp = callPackage ./spandsp { };
+    freeswitch = callPackage ./freeswitch {
+      inherit sofia_sip spandsp;
+      libopusenc = libopusenc.overrideAttrs (oA: {
+        postFixup = ''
+          sed -i 's_opus.h_opus/opus.h_g' $dev/include/opus/opusenc.h
+        '';
+      });
+    };
+  };
+}
